422cedecc5b8eed7f8d249afde77761fa7197189
croes <glenncroes@gmail.com>
2014-01-05 14:22:59 +0100

    -changed directory structure to maven's -added CsvToDataTask

---
+++ docproc-bpm/src/main/java/CsvToDataTask.java
@@ -0,0 +1,38 @@
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+
+public class CsvToDataTask implements JavaDelegate {
+    
+    private static Logger logger = LoggerFactory.getLogger(MyUnitTest.class);
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        String csv = (String) execution.getVariable("data");
+        List<HashMap<String, String>> tasks = new ArrayList<HashMap<String, String>>();
+        CSVReader reader = new CSVReader(new StringReader(csv), ';');
+        String[] nextLine;
+        String[] headers = reader.readNext();
+        while((nextLine = reader.readNext()) != null){
+            HashMap<String, String> taskParams = new HashMap<String, String>();
+            for(int i=0; i<nextLine.length; i++){
+                taskParams.put(headers[i], nextLine[i]);
+                logger.info("Read param '{}' with value '{}'", headers[i], nextLine[i]);
+            }
+            tasks.add(taskParams);
+        }
+        reader.close();
+        execution.setVariable("tasks", tasks);
+        logger.info("csv-to-task: Read {} tasks.", tasks.size());
+    }
+
+}
