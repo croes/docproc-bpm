c876d36f65dd14c16ab9d619e50258f6c5cc49e2
croes <glenncroes@gmail.com>
2014-01-27 15:30:11 +0100

    User credentials are now stored in session. If a REST call is made with a valid session ID cookie, the session credentials are used to auth with the REST the API. If no valid session ID is present, HTTP basic auth is used.

---
+++ docproc-bpm/src/main/java/be/gcroes/thesis/docproc/filter/LoginFilter.java
@@ -0,0 +1,50 @@
+package be.gcroes.thesis.docproc.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.catalina.realm.GenericPrincipal;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoginFilter implements Filter{
+    
+    private static Logger logger = LoggerFactory.getLogger(LoginFilter.class);
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        
+        HttpSession session = httpRequest.getSession();
+        GenericPrincipal principal = (GenericPrincipal) httpRequest.getUserPrincipal();
+        String user = principal.getName();
+        String password = principal.getPassword();
+        if (user != null && session.getAttribute("user") == null) {
+            logger.info("NEW SESSION DETECTED, SAVING USER {} WITH PASS {}", user, password);
+            session.setAttribute("user", user);
+            session.setAttribute("password", password);
+            // First-time login. You can do your intercepting thing here.
+            //auth with REST API as well  
+        }
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {
+        // TODO Auto-generated method stub
+        
+    }
+}
