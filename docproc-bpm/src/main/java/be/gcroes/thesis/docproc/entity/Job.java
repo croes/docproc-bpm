e2b80e08cb166213a0eefaa961ce73296fa35803
croes <glenncroes@gmail.com>
2014-01-30 19:06:55 +0100

    Added DB entities for storing job history info

---
+++ docproc-bpm/src/main/java/be/gcroes/thesis/docproc/entity/Job.java
@@ -0,0 +1,76 @@
+package be.gcroes.thesis.docproc.entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Jobs")
+public class Job implements Serializable {
+
+    private static final long serialVersionUID = -1644990722029309133L;
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name="id")
+    private int id;
+    
+    @Column(name="activiti_job_id")
+    private String activitiJobId;
+    
+    @OneToMany(mappedBy="job", cascade=CascadeType.ALL)
+    private List<Task> tasks = new ArrayList<Task>();
+    
+    public Job(){
+       
+    }
+    
+    public Job(String activitiJobId){
+        this.activitiJobId = activitiJobId;
+    }
+
+    public String getActivitiJobId() {
+        return activitiJobId;
+    }
+
+    public void setActivitiJobId(String activitiJobId) {
+        this.activitiJobId = activitiJobId;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+    
+    public void addTask(Task task){
+        tasks.add(task);
+        if(task.getJob() != this){
+            task.setJob(this);
+        }
+    }
+    
+    public void removeTask(Task task){
+        tasks.remove(task);
+    }
+    
+    public void addTasks(List<Task> tasks){
+        tasks.addAll(tasks);
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+}
---
+++ docproc-bpm/src/test/java/HibernateTest.java
@@ -0,0 +1,71 @@
+import static org.fest.assertions.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import be.gcroes.thesis.docproc.entity.EntityManagerUtil;
+import be.gcroes.thesis.docproc.entity.Job;
+import be.gcroes.thesis.docproc.entity.Task;
+
+
+public class HibernateTest{
+    
+    private EntityManager em;
+    
+    @Before
+    public void beforeEach()
+    {
+       em = EntityManagerUtil.getTestEntityManagerFactory().createEntityManager();
+    }
+
+    @After
+    public void afterEach()
+    {
+       em.close();
+    }
+
+    @Test
+    public void testPersist()
+    {
+       EntityTransaction transaction = em.getTransaction();
+       transaction.begin();
+       
+       Job job1 = new Job();
+       Task t1 = new Task(job1, "Finished");
+       Task t2 = new Task(job1);
+
+       // IDs start as null
+       assertEquals(0, job1.getId());
+       assertEquals(0, t1.getId());
+       assertEquals(0, t2.getId());
+
+       em.persist(job1);
+       
+       Task t3 = new Task(job1);
+       t3.addParam("arg0", "test");
+       t3.addParam("arg1", "testing");
+
+       transaction.commit();
+
+       System.out.println("Job 1");
+       System.out.println("Generated ID is: " + job1.getId());
+       
+       List<Job> jobs = (List<Job>)em.createQuery("SELECT j FROM Job j").getResultList();
+       Job job = jobs.get(0);
+       assertThat(job.getTasks()).hasSize(3);
+       assertThat(job.getTasks()).contains(t1, t2, t3);
+       assertThat(job.getTasks().get(2)).isSameAs(t3);
+       assertThat(job.getTasks().get(2).getParamKeys()).contains("arg0", "arg1");
+       assertThat(job.getTasks().get(2).getParamValues()).contains("test", "testing");
+       assertThat(job.getTasks().get(2).getParams().get("arg0")).isNotNull();
+       assertThat(job.getTasks().get(2).getParams().get("arg0")).isEqualTo("test");
+       
+    }
+ }
