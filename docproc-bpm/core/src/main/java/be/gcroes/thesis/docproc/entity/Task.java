31c8703e3057d4e52d3d773acccf6933cfbeecb5
croes <glenncroes@gmail.com>
2014-04-17 11:02:17 +0200

    changed Task POJO to no longer use Activiti

--- docproc-bpm/core/src/main/java/be/gcroes/thesis/docproc/entity/Task.java
+++ docproc-bpm/core/src/main/java/be/gcroes/thesis/docproc/entity/Task.java
@@ -34,36 +34,44 @@ public class Task implements Serializable{
     @JoinColumn(name="jobid")
     private Job job;
     
-    @Column(name="task_result", columnDefinition="TEXT")
-    private String result;
+    @Column(name="filledtemplate", columnDefinition="TEXT")
+    private String filledTemplate;
+    
+    @Column(name="result", columnDefinition="BLOB")
+    private byte[] result;
     
     @ElementCollection(fetch=FetchType.EAGER)
-    @MapKeyColumn(name="param_key", columnDefinition="TEXT", insertable=false, updatable=false)
+    @MapKeyColumn(name="param_key",
+    			  columnDefinition="TEXT",
+    			  insertable=false,
+    			  updatable=false)
     @CollectionTable(name="task_param",
                     joinColumns=@JoinColumn(name="taskid"))
     @Column(name="param_value", columnDefinition="TEXT")
     private Map<String, String> params = new HashMap<String, String>();
     
-    public Task(){}
+    public Task(){
+    	
+    }
     
-    public Task(Job job, String result, Map<String, String> params){
+    public Task(Job job, Map<String, String> params){
         this.job = job;
-        this.result = result;
+        this.result = new byte[1024];
         this.params = params;
         job.addTask(this);
     }
     
-    public Task(Job job, String result){
+    public Task(Job job){
         this.job = job;
-        this.result = result;
+        this.result = new byte[1024];
         job.addTask(this);
     }
-    
-    public Task(Job job){
-        this(job, "NO RESULT");
-    }
 
-    public Job getJob() {
+    public int getId() {
+	    return id;
+	}
+
+	public Job getJob() {
         return job;
     }
 
@@ -71,16 +79,20 @@ public class Task implements Serializable{
         this.job = job;
     }
 
-    public String getResult() {
+    public String getFilledTemplate() {
+		return filledTemplate;
+	}
+
+	public void setFilledTemplate(String filledTemplate) {
+		this.filledTemplate = filledTemplate;
+	}
+
+	public byte[] getResult() {
         return result;
     }
 
-    public void setResult(String result) {
+    public void setResult(byte[] result) {
         this.result = result;
-    }
-
-    public int getId() {
-        return id;
     }
 
     public Map<String, String> getParams() {
